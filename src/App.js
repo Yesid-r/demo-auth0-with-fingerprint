029ba4fc0e78c4b31e7b6ded42d0aa5a565051d8
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-03-08 11:18:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+import LoginButton from './components/LoginButton'
+import Profile from './components/Profile'
+import LogoutButton from './components/LogoutButton'
+const App = () => {
 
-function App() {
+  const {isAuthenticated, isLoading } = useAuth0()
+  if(isLoading) return <h1>Loading....</h1>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>App
+      {isAuthenticated ? <LogoutButton/> : <LoginButton/>}
+      
+      <Profile/>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
